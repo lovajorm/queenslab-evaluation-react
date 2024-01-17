f7aac96eaf9d0854e011563a968cdaf98e0447f3
Lova Jormfeldt <lova.jormfeldt@consid.se>
2024-01-17 15:52:32 +0100

    creditcard styling

--- src/credit-card/CreaditCardForm.tsx
+++ src/credit-card/CreaditCardForm.tsx
@@ -1,10 +1,8 @@
-import { Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import { Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap"
 
 const CreditCardForm = () => {
-  let months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  let years = [
-    2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034,
-  ];
+  let months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  let years = [2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034]
   return (
     <Form>
       <FormGroup>
@@ -15,37 +13,31 @@ const CreditCardForm = () => {
         <Label for="cardName">Card Name</Label>
         <Input id="cardName" name="cardName" type="text" />
       </FormGroup>
-      <Row>
-        <Col>
-          <FormGroup>
-            <Label for="month">Expiration Date</Label>
-            <Input id="month" name="month" type="select">
-              {months.map((month) => (
-                <option>{month}</option>
-              ))}
-            </Input>
-          </FormGroup>
-        </Col>
-        <Col>
-          <FormGroup>
-            <Label for="year"></Label>
-            <Input id="year" name="year" type="select">
-              {years.map((year) => (
-                <option>{year}</option>
-              ))}
-            </Input>
-          </FormGroup>
-        </Col>
-        <Col>
-          <FormGroup>
-            <Label for="cvv">CVV</Label>
-            <Input id="cvv" name="cvv" type="text" />
-          </FormGroup>
-        </Col>
-      </Row>
+      <div className="d-flex">
+        <Label for="month">Expiration Date</Label>
+        <FormGroup className="month d-flex">
+          <Input id="month" name="month" type="select">
+            {months.map((month) => (
+              <option>{month}</option>
+            ))}
+          </Input>
+          {/* </FormGroup>
+        <FormGroup className="year"> */}
+          {/* <Label for="year">hej</Label> */}
+          <Input id="year" name="year" type="select">
+            {years.map((year) => (
+              <option>{year}</option>
+            ))}
+          </Input>
+        </FormGroup>
+        <FormGroup className="form-group-cvv">
+          <Label for="cvv">CVV</Label>
+          <Input id="cvv" name="cvv" type="text" />
+        </FormGroup>
+      </div>
       <Button color="primary">Submit</Button>
     </Form>
-  );
-};
+  )
+}
 
-export default CreditCardForm;
+export default CreditCardForm
--- src/credit-card/CreditCardImage.tsx
+++ src/credit-card/CreditCardImage.tsx
@@ -3,27 +3,27 @@ const CreditCardImage = () => {
     <div className="credit-card-card m-3 d-flex flex-column justify-content-between">
       <div className="credit-card-card-logo d-flex justify-content-between">
         <div>Debit</div>
-        <img height="25px" src="/assets/visa.png" alt="Visa logo" />
+        <img src="/assets/visa.png" alt="Visa logo" />
       </div>
 
       <div className="credit-card-card-chip">
-        <img width="42px" src="/assets/chip.png" alt="Visa logo" />
+        <img src="/assets/chip.png" alt="Visa logo" />
       </div>
 
       <div className="credit-card-card-number">1234 5678 9012 3456</div>
 
       <div className="credit-card-card-details d-flex justify-content-between">
-        <div className="credit-card-card-holder d-flex flex-column">
+        <div className="d-flex flex-column">
           <div className="credit-card-holder">Card Holder</div>
           <div className="credit-card-name">Lova Jormfedlt</div>
         </div>
         <div className="credit-card-card-expiry">
-          <span>Expires</span>
+          <span className="expires">Expires</span>
           <span className="date">12/34</span>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CreditCardImage;
+export default CreditCardImage
